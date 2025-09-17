794adea601167cc34ce2f768f31ad07e61dbbcbc
chenxy <chxy517@bu.edu>
2025-09-17 15:31:44 -0400

    update practice problems

---
+++ lab3_problemA.cpp
@@ -0,0 +1,18 @@
+#ifndef LAB3_PROBLEMA_H
+#define LAB3_PROBLEMA_H
+
+/**
+ * Given an array of integers, find the length of the longest
+ * contiguous subarray that is strictly increasing.
+ *
+ * For example:
+ *   arr = {1, 2, 2, 3, 4}
+ *   Longest increasing subarray = {2, 3, 4}, length = 3
+ *
+ * @param arr integer array
+ * @param size length of array
+ * @return length of the longest strictly increasing contiguous subarray
+ */
+int longestIncreasingSubarray(int* arr, int size);
+
+#endif
