ce08d6dc731ee94881ed1539d5bd72dd7cb6a446
KevinCxy <84394630+Chenxy517@users.noreply.github.com>
2025-09-21 21:11:41 -0400

    Create test_problemA.cpp

---
+++ test_problemA.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "lab3_problemA.h"
+
+int main() {
+    int arr1[] = {1, 2, 2, 3, 4};
+    int size1 = sizeof(arr1) / sizeof(arr1[0]);
+    std::cout << "Test 1: "
+              << longestIncreasingSubarray(arr1, size1) << " (expected 3)\n";
+
+    int arr2[] = {5, 6, 7, 8, 9};
+    int size2 = sizeof(arr2) / sizeof(arr2[0]);
+    std::cout << "Test 2: "
+              << longestIncreasingSubarray(arr2, size2) << " (expected 5)\n";
+
+    int arr3[] = {10, 9, 8, 7};
+    int size3 = sizeof(arr3) / sizeof(arr3[0]);
+    std::cout << "Test 3: "
+              << longestIncreasingSubarray(arr3, size3) << " (expected 1)\n";
+
+    return 0;
+}
